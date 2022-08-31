34a864d5a9badc46e6f2f6b7a4896bce57d00ced
Gert Muller <gert.muller@abbott.com>
2022-08-31 10:18:40 -0500

    small comment change to verify git working

--- RRRduino_Clinic_Tulsa_2021/Code/013_Servo_by_OOP_Lights_On/013_Servo_by_OOP_Lights_On.ino
+++ RRRduino_Clinic_Tulsa_2021/Code/013_Servo_by_OOP_Lights_On/013_Servo_by_OOP_Lights_On.ino
@@ -1,21 +1,21 @@
 /*
  * Tulsa 2021.10.08 Arduino for Beginners Make and Take Clinic
- * 
+ *
  * When the boom is going down, simulated with a pin going low on pin 8, or the light on a photoresistor indicating occupancy,
  *   turn the tip of the boom's light on, and flash the two other lights on the boom (as well as the ones under the hoods).
- *   
+ *
  *   Making an object with the servo, so it can slowly move between two positions.
- *   
+ *
  *   Keep the lights on until the servo is at the top
- *   
+ *
  *   Enable sound pin
- * 
+ *
  * Servo into a "tilde" pin, like D6
- * 
+ *
  * Author: Speed Muller, Date: 2021.10.08, 08:10
  */
 
-// include the Servo library
+// Include the Servo library
 #include <Servo.h>
 
 #include "light.h"
@@ -57,7 +57,7 @@
 #define TIMEOUT         1000
 
 // 10-bit A/D, 0-1023
-#define MAXPOINT        1023  
+#define MAXPOINT        1023
 #define SETPOINT         250
 
 Light light1( LED1, ONTIME1, OFFTIME1 );
@@ -65,7 +65,7 @@ Light light2( LED2, ONTIME2, OFFTIME2 );
 Light light3( LED3, ONTIME5, OFFTIME5 );
 Light light4( LED4, ONTIME5, OFFTIME5 );
 
-// TulsaServo( uint8_t theServoPin, uint16_t theTime, uint8_t theUpPosition, uint8_t theDownPosition ) 
+// TulsaServo( uint8_t theServoPin, uint16_t theTime, uint8_t theUpPosition, uint8_t theDownPosition )
 
 TulsaServo boomServo( SERVOPIN, SERVOUPDATETIME, 80, 200 );
 
@@ -78,22 +78,22 @@ void setup( ) {
   Serial.begin( BAUD );
 
   pinMode( INPUTPIN, INPUT_PULLUP );
-  pinMode( ANAPIN, INPUT );  
+  pinMode( ANAPIN, INPUT );
   pinMode( SOUNDPIN, OUTPUT );
 
   boomServo.begin( );
-  
+
   light1.begin( );
   light2.begin( );
   light3.begin( );
-  delay( ONTIME5 );  
+  delay( ONTIME5 );
   light4.begin( );
 
   Serial.println( );
   Serial.println( VERSION_STR );
 
   now = before = millis( );
-  boomServo.raise( );  
+  boomServo.raise( );
 } // setup( )
 
 
@@ -104,7 +104,7 @@ void loop( ) {
     analogValue = analogRead( ANAPIN );
     Serial.println( analogValue );
   } // if time to read
-  
+
   if( ( digitalRead( INPUTPIN ) == LOW ) || ( analogValue < SETPOINT ) ) {
     light2.poweron( );
     light3.poweron( );
@@ -116,11 +116,11 @@ void loop( ) {
     if( boomServo.atTop( ) == true ) {
       light2.poweroff( );
       light3.poweroff( );
-      light4.poweroff( );  
+      light4.poweroff( );
       digitalWrite( SOUNDPIN, LOW );
     } // if done
   } // if pin is active (which is LOW )
-  
+
   light1.update( );
   light2.update( );
   light3.update( );
@@ -128,5 +128,5 @@ void loop( ) {
   boomServo.update( );
 } // loop( )
 
-// Can do better than ALWAYS telling it to raise or lower it. 
+// Can do better than ALWAYS telling it to raise or lower it.
 // Create a state and set or reset it.
