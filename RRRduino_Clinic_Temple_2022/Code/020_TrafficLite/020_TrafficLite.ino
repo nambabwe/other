e004f42a60eebb9367ab4ac4a982d700ebe59398
Gert Muller aka Speed <nambabwe@users.noreply.github.com>
2023-10-24 21:27:24 -0500

    Update 020_TrafficLite.ino
    typo in comment

--- RRRduino_Clinic_Temple_2022/Code/020_TrafficLite/020_TrafficLite.ino
+++ RRRduino_Clinic_Temple_2022/Code/020_TrafficLite/020_TrafficLite.ino
@@ -2,7 +2,7 @@
 
 #include "TrafficLite.h"
 
-// We are lazy, putting 2, 3, 4, etc straigh in...
+// We are lazy, putting 2, 3, 4, etc straight in...
 TrafficLite *trafficLiteNS = new TrafficLite( 2,  3,  4, HIGH ); // create the first TrafficLite object using pins 2, 3 and 4 
 TrafficLite *trafficLiteEW = new TrafficLite( 5,  6,  7, HIGH ); // create the other TrafficLite object using pins 5, 6 and 7
 
